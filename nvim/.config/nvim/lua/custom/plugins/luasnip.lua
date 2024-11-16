8e7ecdbca81da9f3c80eef467034e596264766b2
Fredrik Glawe <fredrik@neuraldsp.com>
2024-11-16 20:43:23 +0100

    Added a few snippets

--- nvim/.config/nvim/lua/custom/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/custom/plugins/luasnip.lua
@@ -4,4 +4,7 @@ return {
   version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   -- install jsregexp (optional!).
   build = 'make install_jsregexp',
+  config = function()
+    require('luasnip.loaders.from_snipmate').lazy_load()
+  end,
 }
