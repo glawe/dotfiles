3f630a5b9d52b47bc086de5c28c2f21d236315b3
Fredrik Glawe <fredrik@neuraldsp.com>
2024-11-17 11:36:21 +0100

    Added nvim-highlight-colors

---
+++ nvim/.config/nvim/lua/custom/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,12 @@
+return {
+  'brenoprata10/nvim-highlight-colors',
+  config = function()
+    require('nvim-highlight-colors').setup {
+      render = 'virtual',
+      virtual_symbol = '',
+      virtual_symbol_prefix = '',
+      virtual_symbol_suffix = ' ',
+      virtual_symbol_position = 'inline',
+    }
+  end,
+}
