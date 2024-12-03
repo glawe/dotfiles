6ad7b0ea343261d3cbbf8303a47cfc9d64af00fa
Fredrik Glawe <fredrik@neuraldsp.com>
2024-12-03 21:02:34 +0100

    updated blink cfg

--- nvim/.config/nvim/lua/custom/plugins/blink.cmp.lua
+++ nvim/.config/nvim/lua/custom/plugins/blink.cmp.lua
@@ -2,7 +2,8 @@ return {
   'saghen/blink.cmp',
   lazy = false, -- lazy loading handled internally
   -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  --dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = 'L3MON4D3/LuaSnip',
 
   -- use a release tag to download pre-built binaries
   version = 'v0.*',
@@ -10,6 +11,7 @@ return {
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
+
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -18,34 +20,18 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    --keymap = { preset = 'default' },
-    keymap = {
-      preset = 'enter',
+    keymap = { preset = 'super-tab' },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono',
     },
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = false,
-    },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'mono',
-    windows = {
-      autocomplete = {
-        --draw = 'reversed',
-        winblend = vim.o.pumblend,
-        draw = {
-          columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind', gap = 1 } },
-        },
-      },
-      documentation = {
-        auto_show = true,
-      },
-      ghost_text = {
-        enabled = vim.g.ai_cmp,
-      },
-    },
+
     -- default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, via `opts_extend`
     sources = {
@@ -54,11 +40,20 @@ return {
       },
     },
 
+    completion = {
+      menu = {
+        draw = {
+          treesitter = true,
+          columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind', gap = 1 } },
+        },
+      },
+    },
+
     -- experimental auto-brackets support
-    accept = { auto_brackets = { enabled = true } },
+    -- completion = { accept = { auto_brackets = { enabled = true } } }
 
     -- experimental signature help support
-    trigger = { signature_help = { enabled = true } },
+    -- signature = { enabled = true }
   },
   -- allows extending the enabled_providers array elsewhere in your config
   -- without having to redefine it
